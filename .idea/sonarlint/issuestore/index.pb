3ecb279bbe8c4a4ffedb13301f18a40e4176890b
alekskiseleva <sasha4800309@hotmail.com>
2023-08-23 21:51:00 +0300

    commit3

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,5 @@
 
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+[
++src/main/java/homework7/transports/Car.java,a\a\aaa34ed49465f29de07b77cdf28a419159649207
---
+++ src/main/java/homework9/Employee.java
@@ -0,0 +1,30 @@
+package homework9;
+
+/*
+(5) Создайте класс Сотрудник с полями: имя, возраст
+*/
+public class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
